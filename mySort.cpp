8c942a5008657a8fbbe327fd7bfdfc2eae0937ee
Smit Lila <smitlila7@gmail.com>
2024-02-29 00:58:45 -0800

    Update mySort.cpp

--- mySort.cpp
+++ mySort.cpp
@@ -1,10 +1,3 @@
-// Smit Lila
-// Class (CECS 325-02)
-// Project Name (Prog 5 - Threads)
-// Due Date (11/02/2023)
-//
-// I certify that this program is my own original work. I did not copy any part of this program from
-// any other source. I further certify that I typed each and every line of code in this program.
 #include <string>
 #include <thread> 
 #include <iostream>
